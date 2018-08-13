68d28a5d359f9ee2e5f3d26bfa5f15cea5b8c9b8
elong <elongliks@gmail.com>
2018-08-13 16:45:20 +0300

    Added parentheses support.

--- ass4/ass4.py
+++ ass4/ass4.py
@@ -1,6 +1,3 @@
-"""
-Add parentheses support.
-"""
 import re
 import math
 import ast
@@ -12,13 +9,13 @@ def input_validation(exp):
 
 
 def evaluate_literal_types(exp_list):
-    operations = ['+', '-', '*', '/', '^', '!', '%', '&', '$', '@']
+    operations = ['+', '-', '*', '/', '^', '!', '%', '&', '$', '@', '[', ']']
     return [ast.literal_eval(operand) if operand not in operations else operand for operand in exp_list]
 
 
 def exp_to_list(exp):
     exp_list = re.findall(
-        r'[@&!*/^%$+-]|~?\d+(?:\.\d*)?|~?\.\d+', exp.replace(' ', ''))
+        r'[\[\]@&!*/^%$+-]|~?\d+(?:\.\d*)?|~?\.\d+', exp.replace(' ', ''))
     return [e.replace('~', '-') for e in exp_list]
 
 
@@ -26,14 +23,28 @@ def closest_operation(exp_list, operation_group):
     return min(exp_list.index(op) if op in exp_list else math.inf for op in operation_group)
 
 
+def list_rindex(alist, value):
+    return len(alist) - list(reversed(alist)).index(value) - 1
+
+
 def evaluate_operation(exp_list, op_pos):
     operation = exp_list[op_pos]
     left_op = exp_list[op_pos - 1]
     right_op = exp_list[op_pos + 1] if len(exp_list) > op_pos + 1 else None
+
+    if operation == ']':
+        opening_bracket_pos = list_rindex(exp_list[:op_pos], '[')
+        evaluated_expression = evaluate_list(
+            exp_list[opening_bracket_pos+1:op_pos])
+        exp_list[op_pos] = evaluated_expression
+        del exp_list[opening_bracket_pos:op_pos]
+        return
+
     if operation == '!':
         exp_list[op_pos] = math.factorial(left_op)
         del exp_list[op_pos-1]
         return
+
     elif operation == '&':
         exp_list[op_pos] = min(left_op, right_op)
     elif operation == '$':
@@ -56,26 +67,24 @@ def evaluate_operation(exp_list, op_pos):
 
 
 def evaluate_list(exp_list):
-    precedence = [('+', '-'), ('*', '/'), ('^'), ('!', '%'), ('&', '$', '@')]
+    precedence = [('+', '-'), ('*', '/'),
+                  ('^'), ('!', '%'), ('&', '$', '@'), (']')]
     for operation_group in reversed(precedence):
         while closest_operation(exp_list, operation_group) != math.inf:
             operation = closest_operation(exp_list, operation_group)
             evaluate_operation(exp_list, operation)
-    return exp_list
+    return exp_list[0]
 
 
 def main():
-    print('Please enter your algebraic expression.')
+    print('Please enter your arithmetic expression.')
     while True:
         expression = input('>>> ')
-        try:
-            input_validation(expression)
-            expression_list = exp_to_list(expression)
-            expression_list = evaluate_literal_types(expression_list)
-            evaluate_list(expression_list)
-            print(*expression_list)
-        except (TypeError, ValueError):
-            print("Please make sure you're entering a valid expression.")
+        input_validation(expression)
+        expression_list = exp_to_list(expression)
+        expression_list = evaluate_literal_types(expression_list)
+        evaluate_list(expression_list)
+        print(*expression_list)
 
 
 if __name__ == '__main__':
